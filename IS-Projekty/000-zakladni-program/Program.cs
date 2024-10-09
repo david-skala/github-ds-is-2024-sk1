349d78a112ea5fc712e3ab8e689f18c07204c667
David Skála <david.skala@tul.cz>
2024-10-09 09:44:54 +0200

    #1 zakladni kod s opakovanim

---
+++ IS-Projekty/000-zakladni-program/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program {
+    static void Main() {
+
+        string again = "a";
+
+        while(again == "a") {
+
+        Console.Clear();
+        Console.WriteLine("**************************");
+        Console.WriteLine("**** Výpis řady čísel ****");
+        Console.WriteLine("**************************");
+        Console.WriteLine("****** David Skála *******");
+        Console.WriteLine("**************************");
+        Console.WriteLine();
+
+
+
+        Console.WriteLine();
+        Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+        again = Console.ReadLine();
+
+        }
+    }
+}
+
+// jednoradkovy komentar
+
+/* vice radkovy
+komentar Shift+Alt+A */
