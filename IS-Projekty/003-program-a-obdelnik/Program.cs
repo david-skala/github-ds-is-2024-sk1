f5fe85ab88453d6023307b441da39416db4dd643
David Skála <david.skala@tul.cz>
2024-10-16 10:24:08 +0200

    #3 uprava

--- IS-Projekty/003-program-a-obdelnik/Program.cs
+++ IS-Projekty/003-program-a-obdelnik/Program.cs
@@ -39,6 +39,7 @@ string again = "a";
         for(int i = 1; i <= height; i++) {
             for(int j = 1; j <= width; j++) {
                 Console.Write("* ");
+                System.Threading.Thread.Sleep(System.TimeSpan.FromMilliseconds(100));
             }
 
             Console.WriteLine();
