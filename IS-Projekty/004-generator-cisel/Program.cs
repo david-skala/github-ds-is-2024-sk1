79fc4e50791ce9ad58a4c9da7d4d787790caf3de
David Skála <david.skala@tul.cz>
2024-10-23 09:21:17 +0200

    #4 generator zaklad

--- IS-Projekty/004-generator-cisel/Program.cs
+++ IS-Projekty/004-generator-cisel/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+
+string again = "a";
+
+        while(again == "a") {
+
+        Console.Clear();
+        Console.WriteLine("*******************************************");
+        Console.WriteLine("******** GENERÁTOR NÁHODNÝCH ČÍSEL ********");
+        Console.WriteLine("*******************************************");
+        Console.WriteLine("*************** David Skála ***************");
+        Console.WriteLine("*******************************************");
+        Console.WriteLine();
+
+        Console.Write("Zadejte počet generovaných čísel:");
+        int n;
+        while(!int.TryParse(Console.ReadLine(), out n)) {
+            Console.Write("Nezadali jste celé číslo. Zadejte znovu: ");
+        }
+
+        Console.Write("Zadejte dolní mez:");
+        int dm;
+        while(!int.TryParse(Console.ReadLine(), out dm)) {
+            Console.Write("Nezadali jste celé číslo. Zadejte znovu: ");
+        }
+
+        Console.Write("Zadejte horní mez:");
+        int hm;
+        while(!int.TryParse(Console.ReadLine(), out hm)) {
+            Console.Write("Nezadali jste celé číslo. Zadejte znovu: ");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("*******************************************");
+        Console.WriteLine("Zadané hodnoty:");
+        Console.WriteLine("Počet čísel: {0}; dolní mez: {1}; horní mez {2}", n, dm, hm);
+        Console.WriteLine("*******************************************");
+        Console.WriteLine();
+
+        Console.WriteLine();
+        Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+        again = Console.ReadLine();
+
+        }
