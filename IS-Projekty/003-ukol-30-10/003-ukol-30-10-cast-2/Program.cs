4c4284c9026e30fd3b6f4934854e0bfcf57bce91
David Skála <david.skala@tul.cz>
2024-10-29 22:08:51 +0100

    #3 ukol program 2

---
+++ IS-Projekty/003-ukol-30-10/003-ukol-30-10-cast-2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Globalization;
+
+string again = "a";
+
+        while(again == "a") {
+
+        Console.Clear();
+        Console.WriteLine("**************************");
+        Console.WriteLine("****** Úkol výpis 2 ******");
+        Console.WriteLine("**************************");
+        Console.WriteLine("****** David Skála *******");
+        Console.WriteLine("**************************");
+        Console.WriteLine();
+
+        // Zadání hodnot do programu
+
+        Console.Write("Zadejte výšku obrazce (celé číslo): ");
+        int height;
+        while(!int.TryParse(Console.ReadLine(), out height)) {
+            Console.Write("Nezadali jste celé číslo. Zadejte znovu výšku obrazce: ");
+        }
+
+        for (int j = 1; j <= height; j++)
+        {
+            Console.Write("* ");
+            System.Threading.Thread.Sleep(System.TimeSpan.FromMilliseconds(100));
+        }
+        Console.WriteLine();
+
+        for (int i = 1; i <= height - 2; i++)
+        {
+            Console.Write("* ");
+            System.Threading.Thread.Sleep(System.TimeSpan.FromMilliseconds(100));
+            Console.WriteLine();
+        }
+
+        for (int j = 1; j <= height; j++)
+        {
+            Console.Write("* ");
+            System.Threading.Thread.Sleep(System.TimeSpan.FromMilliseconds(100));
+        }
+        Console.WriteLine();
+
+        for (int i = 1; i <= height - 2; i++)
+        {
+            Console.WriteLine();
+            for (int j = 1; j <= height - 1; j++)
+            {
+                Console.Write("  ");
+            }
+            Console.Write("* ");
+            System.Threading.Thread.Sleep(System.TimeSpan.FromMilliseconds(100));
+        }
+
+        Console.WriteLine();
+        for (int j = 1; j <= height; j++)
+        {
+            Console.Write("* ");
+            System.Threading.Thread.Sleep(System.TimeSpan.FromMilliseconds(100));
+        }
+        Console.WriteLine();
+
+        Console.WriteLine();
+        Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+        again = Console.ReadLine();
+
+        }
