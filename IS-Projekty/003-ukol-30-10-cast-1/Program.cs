3577dae598dd7c8d0f65fb9c0c54aa1fb5b41a4e
David Skála <david.skala@tul.cz>
2024-10-29 21:59:39 +0100

    #3 ukol prvni program

---
+++ IS-Projekty/003-ukol-30-10-cast-1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+
+string again = "a";
+
+        while(again == "a") {
+
+        Console.Clear();
+        Console.WriteLine("**************************");
+        Console.WriteLine("******** Obdelník ********");
+        Console.WriteLine("**************************");
+        Console.WriteLine("****** David Skála *******");
+        Console.WriteLine("**************************");
+        Console.WriteLine();
+
+        // Zadání hodnot do programu
+
+        Console.Write("Zadejte výšku obrazce (celé číslo): ");
+        int height;
+        while(!int.TryParse(Console.ReadLine(), out height)) {
+            Console.Write("Nezadali jste celé číslo. Zadejte znovu výšku obrazce: ");
+        }
+
+for (int j = 1; j <= height; j++)
+        {
+            Console.Write("* ");
+            System.Threading.Thread.Sleep(System.TimeSpan.FromMilliseconds(100));
+        }
+        Console.WriteLine();
+
+        for (int i = 1; i <= height - 2; i++)
+        {
+            for (int j = 1; j <= height - i - 1; j++)
+            {
+                Console.Write("  "); // Print spaces
+            }
+            Console.Write("* ");
+            System.Threading.Thread.Sleep(System.TimeSpan.FromMilliseconds(100));
+            Console.WriteLine();
+        }
+
+        for (int j = 1; j <= height; j++)
+        {
+            Console.Write("* ");
+            System.Threading.Thread.Sleep(System.TimeSpan.FromMilliseconds(100));
+        }
+        Console.WriteLine();
+
+        Console.WriteLine();
+        Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+        again = Console.ReadLine();
+
+        }
