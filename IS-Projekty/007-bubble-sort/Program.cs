6028d729fb47b66227c7631da4ea472651672775
David Skála <david.skala@tul.cz>
2024-11-13 09:30:12 +0100

    #7 razeni

---
+++ IS-Projekty/007-bubble-sort/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Globalization;
+
+string again = "a";
+
+        while(again == "a") {
+
+        Console.Clear();
+        Console.WriteLine("*******************************************");
+        Console.WriteLine("******** GENERÁTOR NÁHODNÝCH ČÍSEL ********");
+        Console.WriteLine("*******************************************");
+        Console.WriteLine("*************** David Skála ***************");
+        Console.WriteLine("*******************************************");
+        Console.WriteLine();
+
+        Console.Write("Zadejte počet generovaných čísel:");
+        int n;
+        while(!int.TryParse(Console.ReadLine(), out n)) {
+            Console.Write("Nezadali jste celé číslo. Zadejte znovu: ");
+        }
+
+        Console.Write("Zadejte dolní mez:");
+        int dm;
+        while(!int.TryParse(Console.ReadLine(), out dm)) {
+            Console.Write("Nezadali jste celé číslo. Zadejte znovu: ");
+        }
+
+        Console.Write("Zadejte horní mez:");
+        int hm;
+        while(!int.TryParse(Console.ReadLine(), out hm)) {
+            Console.Write("Nezadali jste celé číslo. Zadejte znovu: ");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("*******************************************");
+        Console.WriteLine("Zadané hodnoty:");
+        Console.WriteLine("Počet čísel: {0}; dolní mez: {1}; horní mez {2}", n, dm, hm);
+        Console.WriteLine("*******************************************");
+        Console.WriteLine();
+
+        // Deklarace pole
+
+        int[] myArray = new int[n];
+
+        Random randomNumber = new Random();
+
+        Console.WriteLine("\nNáhodná čísla:");
+        for(int i = 0; i < n; i++) {
+            myArray[i] = randomNumber.Next(dm, hm + 1);
+            Console.Write("{0}; ", myArray[i]);
+        }
+
+        for(int i = 0; i < n-1; i++) {
+            for(int j = 0; j < n-i-1; j++) {
+                if(myArray[j] < myArray[j+1]) {
+                    int pom = myArray[j+1];
+                    myArray[j+1] = myArray[j];
+                    myArray[j] = pom;
+                }
+            }
+        }
+
+        Console.WriteLine("\nSeřazené pole:");
+        for(int i = 0; i < n; i++) {
+            myArray[i] = randomNumber.Next(dm, hm + 1);
+            Console.Write("{0}; ", myArray[i]);
+        }
+
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+        again = Console.ReadLine();
+
+        }
